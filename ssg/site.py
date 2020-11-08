f464c656c45b4b4922b63cec7a3a10733ab12f06
Thibaut <thibaut.pascual@mailo.com>
2020-11-08 15:29:10 +0100

    Completed module

---
+++ ssg/site.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+class Site:
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        """ Create a Path "directory" containing full path to destination folder
+        """
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build():
+    	self.dest.mkdir(parents=True, exist_ok=True)
+    	for path in self.source.rglob("*"):
+    		if path.is_dir():
+    		    create_dir(path) 
+
+
+
+
+
