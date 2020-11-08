a7def0641e636a3d5d3a52c399086a4778b82a9a
Thibaut <thibaut.pascual@mailo.com>
2020-11-08 16:03:32 +0100

    Completed module

---
+++ ssg/parsers.py
@@ -0,0 +1,15 @@
+from typing import List
+from pathlib import Path
+
+class Parser:
+    extension = []
+    extension = List[str]
+
+    def valid_extension(extension):
+        if is self.extension:
+            return True
+        else:
+            return False
+    
+    def parse(path, source, dest):
+        
